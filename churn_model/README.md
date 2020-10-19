7aace66c2dc78de19bf32cba3e96ee6c2b2be504
cheepong.lee <cheepong.lee@grabtaxi.com>
2020-10-20 05:59:17 +0800

    first kaggle project

---
+++ churn_model/README.md
@@ -0,0 +1,6 @@
+
+# churn model prediction
+
+## LOCAL SETUP via terminal or command line
+1. change file directory to churn_model folder
+2. Run 'python api.py 12345'
---
+++ churn_model/api.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, request, jsonify
+from sklearn.externals import joblib
+import traceback
+import pandas as pd
+import numpy as np
+
+# Your API definition
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if logmodel:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query.loc[df['TotalCharges']==' ', 'TotalCharges'] = df['MonthlyCharges']
+            query['TotalCharges'] = df['TotalCharges'].astype('float64')
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Rerun the jupyter notebook to train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 12345 # If you don't provide any port the port will be set to 12345
+
+    logmodel = joblib.load("model.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
+    app.run(port=port, debug=True)
